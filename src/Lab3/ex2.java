a8f823371511dee683350a06204cb3c1d904a19a
dibbyorocks <dibbyobhattacharjee@gmail.com>
2022-05-31 12:12:28 +0530

    ex 2 update

--- src/Lab3/ex2.java
+++ src/Lab3/ex2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ex2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Mirror Sentence: ");
+        System.out.print("Enter the  Sentence: ");
         String str = sc.next();
         String[] strArray = str.split(" ");
         for (String temp : strArray) {
